78a789ab54c219e483a5bebadec7ebf04933f857
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-06 02:10:09 +0300

    fix typo & move nextFrame into init func

--- src/game.js
+++ src/game.js
@@ -10,7 +10,14 @@ const init = () => {
   let snake = new Snake(options.startX, options.startY);
   snake[options.startDirection]();
   Score.reset();
-  newFrame();
+
+  const nextFrame = () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(nextFrame, options.timeStep);
+    snake.move();
+  };
+
+  nextFrame();
   window.onkeydown = e => {
     let keyMethods = {
       37: 'turnLeft',
@@ -24,12 +31,6 @@ const init = () => {
       nextFrame();
     }
   };
-};
-
-const nextFrame = () => {
-  clearTimeout(timeout);
-  timeout = setTimeout(nextFrame, options.timeStep);
-  snake.move();
 };
 
 const reset = () => {
