e2b8d513dfd34f47bf819b627bf1d2c745d99a08
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-06 01:56:06 +0300

    add game logic :smile:

---
+++ src/game.js
@@ -0,0 +1,53 @@
+import Drawing from './drawing';
+import Snake from './snake';
+import Score from './score';
+import options from './options';
+
+let timeout;
+
+const init = () => {
+  Drawing.reset();
+  let snake = new Snake(options.startX, options.startY);
+  snake[options.startDirection]();
+  Snake.reset();
+  newFrame();
+  window.onkeydown = e => {
+    let keyMethods = {
+      37: 'turnLeft',
+      38: 'turnUp',
+      39: 'turnRight',
+      40: 'turnDown',
+    };
+    let method = keyMethods[e.keyCode];
+    if (method) {
+      snake[method]();
+      nextFrame();
+    }
+  };
+};
+
+const nextFrame = () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(nextFrame, options.timeStep);
+  snake.move();
+};
+
+const reset = () => {
+  clearTimeout(timeout);
+  init();
+};
+
+const die = () => {
+  alert(`💀 💀 💀 💀, YOUR SCORE was ${Score.value}`);
+  reset();
+};
+
+const win = () => {
+  alert(`🎉 🎉 🎉 🎉, YOU WIN!!!!`);
+};
+
+export default {
+  die,
+  win,
+  init,
+};
