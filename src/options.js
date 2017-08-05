2a5707b464d7f3e210a829db7eac796d8ed58f39
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-06 01:03:22 +0300

    add options

---
+++ src/options.js
@@ -0,0 +1,13 @@
+export default {
+  width: 500,
+  height: 500,
+  rows: 25,
+  cols: 25,
+  timeStep: 100,
+  startX: 2,
+  startY: 2,
+  startDirection: 'turnRight',
+  snakeColor: 'red',
+  foodColor: 'yellow',
+  segmentGrowth: 5,
+};
