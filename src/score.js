6212a4accd0b3746d993ede280715ff63aaaf6a5
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-06 01:33:18 +0300

    add score logic :smile:

---
+++ src/score.js
@@ -0,0 +1,32 @@
+const scoreSpan = document.getElementById('score');
+const highScoreSpan = document.getElementById('highScore');
+let score = 0;
+let highScore = +localStorage.getItem('highScore');
+
+const updateHighScore = () => {
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem('highScore', highScore);
+  }
+  highScoreSpan.innerText = highScore;
+};
+
+const increment = () => {
+  score++;
+  scoreSpan.innerText = score;
+  updateHighScore();
+};
+
+const reset = () => {
+  score = 0;
+  scoreSpan.innerText = score;
+  updateHighScore();
+};
+
+const value = () => score;
+
+export default {
+  increment,
+  reset,
+  value,
+};
